ce8d9a8594cc00a2176268dda1f381d96f6a46e6
hafsatahir94 <hafsafazal94@hotmail.com>
2025-02-02 08:07:54 +0500

    please add

--- app.js
+++ app.js
@@ -1,1 +1,73 @@
-alert("welcome to the website");
+// alert("welcome to the website");
+// alert("Java script alert \n \n Error! Please enter a valid password.");
+// alert("Javascript alert\n \n Welcome to the Jsland...\n Happy coding!")
+// alert("Javascript alert\n \n Welcome to the Js land...");
+// alert("Javascript alert \n\n Happy coding!")
+// console.log("Hello.. I can run js through my web browser's console");
+// alert("Hello!");
+// d. Body (after your page’s HTML)
+// var userName;
+// let myName = "Hafsa tahir";
+// console.log(myName);
+// var message;
+// message = "Hello World";
+// alert ("Hello World");
+// var studentName= "johndoe";
+// var studentAge = "16";
+// var studentCourse = "mobile application development";
+// alert(studentName);
+// alert(studentAge);
+// alert(studentCourse);
+// let pizza = "PIZZA\nPIZZ\nPIZ \nPI \nP"
+// console.log(pizza);
+// let email = "hafsafazal94@hotmail.com";
+// alert("my email address is " + email );
+// let book = "A smarter way to learn JavaScript"
+// alert ("I am trying to learn from the book A smarter way to learn JavaScript")
+
+// document.write ("Yah! I can write HTML content through Javascript")
+// let design ="▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬";
+// alert(design);
+// document.write(design);
+
+// let age ="30";
+// alert( "I am "+ age + "years old");
+// let visitorsVisit ="14"
+// alert("You have visited this site "+ visitorsVisit + " times")
+// 
+// let birthYear = 1994 ;
+// document.write ("My birth year is " + birthYear  ) + ("\n")
+// document.write ("Datatype of my birthyear is number")
+// let visitorsName ="johnDoe  "
+// let productOrdered = " ordered 5 t-shirts  "
+// let productTitle = "xyz clothing store"
+// document.write(visitorsName  +   productOrdered +    productTitle);
+// var1 = "hi", var2 = "hey" , var3 = "hello";
+// legalnames
+// var1 = "hi"
+// var2 = "hey"
+// var3 = "hello"
+// var4 = "hmm"
+// var5 = "hehe"
+// illegalnames
+// var 1City = "karachi"
+// var city-1 = "lahore"
+// var city name = "islamabad"
+// var #cityname = "rawalpindi"
+// var var Cityname = "faislabad"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
